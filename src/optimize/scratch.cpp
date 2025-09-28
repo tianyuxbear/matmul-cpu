6f1a208e57c590f5f11da6c593793e909267cbfc
tianyuxbear <tianyuxbear@gmail.com>
2025-09-28 13:48:08 +0800

    perf(scratch): likely branch and static inline

--- src/optimize/scratch.cpp
+++ src/optimize/scratch.cpp
@@ -6,6 +6,9 @@
 
 #define min(x, y) ((x) < (y) ? (x) : (y))
 
+#define likely(x) __builtin_expect(!!(x), 1)
+#define unlikely(x) __builtin_expect(!!(x), 0)
+
 #define MC 840
 #define NC 1024
 #define KC 384
@@ -16,13 +19,13 @@
 static float blockA_packed[MC * KC] __attribute__((aligned(64)));
 static float blockB_packed[NC * KC] __attribute__((aligned(64)));
 
-__mmask16 create_mask(int nr) {
+static inline __mmask16 create_mask(int nr) {
     nr = (nr < 0) ? 0 : (nr > 16) ? 16
                                   : nr;
     return _cvtu32_mask16((1u << nr) - 1);
 }
 
-static void pack_panelA(float *A, float *blockA_packed, int mr, int kc, int K) {
+static inline void pack_panelA(float *A, float *blockA_packed, int mr, int kc, int K) {
     for (int p = 0; p < kc; ++p) {
         for (int i = 0; i < mr; ++i) {
             *blockA_packed++ = A[i * K + p];
@@ -33,14 +36,14 @@ static void pack_panelA(float *A, float *blockA_packed, int mr, int kc, int K) {
     }
 }
 
-static void pack_blockA(float *A, float *blockA_packed, int mc, int kc, int K) {
+static inline void pack_blockA(float *A, float *blockA_packed, int mc, int kc, int K) {
     for (int i = 0; i < mc; i += MR) {
         int mr = min(MR, mc - i);
         pack_panelA(&A[i * K], &blockA_packed[i * kc], mr, kc, K);
     }
 }
 
-static void pack_panelB(float *B, float *blockB_packed, int nr, int kc, int K) {
+static inline void pack_panelB(float *B, float *blockB_packed, int nr, int kc, int K) {
 
     for (int p = 0; p < kc; ++p) {
         for (int j = 0; j < nr; ++j) {
@@ -51,44 +54,44 @@ static void pack_panelB(float *B, float *blockB_packed, int nr, int kc, int K) {
         }
     }
 }
-static void pack_blockB(float *B, float *blockB_packed, int nc, int kc, int K) {
+static inline void pack_blockB(float *B, float *blockB_packed, int nc, int kc, int K) {
     for (int j = 0; j < nc; j += NR) {
         int nr = min(NR, nc - j);
         pack_panelB(&B[j * K], &blockB_packed[j * kc], nr, kc, K);
     }
 }
 
-inline void load_accum(float *C, __m512 C_accum[MR][2], int N, int mr) {
+static inline void load_accum(float *C, __m512 C_accum[MR][2], int N, int mr) {
     for (int i = 0; i < mr; ++i) {
         C_accum[i][0] = _mm512_loadu_ps(&C[i * N]);
         C_accum[i][1] = _mm512_loadu_ps(&C[i * N + 16]);
     }
 }
 
-inline void maskload_accum(float *C, __m512 C_accum[MR][2], int N, int mr, __mmask16 packed_mask_0, __mmask16 packed_mask_1) {
+static inline void maskload_accum(float *C, __m512 C_accum[MR][2], int N, int mr, __mmask16 packed_mask_0, __mmask16 packed_mask_1) {
     for (int i = 0; i < mr; ++i) {
         C_accum[i][0] = _mm512_maskz_loadu_ps(packed_mask_0, &C[i * N]);
         C_accum[i][1] = _mm512_maskz_loadu_ps(packed_mask_1, &C[i * N + 16]);
     }
 }
 
-inline void store_accum(float *C, __m512 C_accum[MR][2], int N, int mr) {
+static inline void store_accum(float *C, __m512 C_accum[MR][2], int N, int mr) {
     for (int i = 0; i < mr; ++i) {
         _mm512_storeu_ps(&C[i * N], C_accum[i][0]);
         _mm512_storeu_ps(&C[i * N + 16], C_accum[i][1]);
     }
 }
 
-inline void maskstore_accum(float *C, __m512 C_accum[MR][2], int N, int mr, __mmask16 packed_mask_0, __mmask16 packed_mask_1) {
+static inline void maskstore_accum(float *C, __m512 C_accum[MR][2], int N, int mr, __mmask16 packed_mask_0, __mmask16 packed_mask_1) {
     for (int i = 0; i < mr; ++i) {
         _mm512_mask_storeu_ps(&C[i * N], packed_mask_0, C_accum[i][0]);
         _mm512_mask_storeu_ps(&C[i * N + 16], packed_mask_1, C_accum[i][1]);
     }
 }
 
-inline void fma_loop(float *blockA_packed, float *blockB_packed,
-                     __m512 C_accum[MR][2], __m512 *a_packedFloat16,
-                     __m512 *b0_packedFloat16, __m512 *b1_packedFloat16, int kc) {
+static inline void fma_loop(float *blockA_packed, float *blockB_packed,
+                            __m512 C_accum[MR][2], __m512 *a_packedFloat16,
+                            __m512 *b0_packedFloat16, __m512 *b1_packedFloat16, int kc) {
     for (int p = 0; p < kc; ++p) {
         *b0_packedFloat16 = _mm512_loadu_ps(blockB_packed);
         *b1_packedFloat16 = _mm512_loadu_ps(blockB_packed + 16);
@@ -128,7 +131,7 @@ static inline void micro_kernel(float *blockA_packed, float *blockB_packed,
     __mmask16 packed_mask_0 = {};
     __mmask16 packed_mask_1 = {};
 
-    if (nr == NR) {
+    if (likely(nr == NR)) {
         load_accum(C, C_accum, N, mr);
         fma_loop(blockA_packed, blockB_packed, C_accum, &a_packedFloat16,
                  &b0_packedFloat16, &b1_packedFloat16, kc);
